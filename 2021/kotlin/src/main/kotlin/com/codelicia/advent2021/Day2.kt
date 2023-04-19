1b58722810d5dee7cd739536ec98046278fdfb45
Jefersson Nathan <malukenho.dev@gmail.com>
2023-04-19 18:24:37 +0100

    feat: Add Day2 implementation and test
    Add implementation and test for Day2 puzzle. The implementation includes
    two methods: `part1()` and `part2()`, which calculate the solution for
    the puzzle. The `Day2` class takes a list of strings that represent
    directions for a submarine to move in. The `part1()` method calculates
    the Manhattan distance for the final position of the submarine, and the
    `part2()` method calculates a different distance based on the orientation
    of the submarine. Both methods were tested using sample input and input
    from a file.

---
+++ 2021/kotlin/src/main/kotlin/com/codelicia/advent2021/Day2.kt
@@ -0,0 +1,46 @@
+package com.codelicia.advent2021
+
+class Day2(private val plannedCourse: List<String>) {
+
+    private val submarine =
+        plannedCourse
+            .map {
+                val (orientation, to) = it.split(" ")
+                orientation to to.toInt()
+            }
+
+    private fun List<Pair<String, Int>>.sumOfCoordinate(coordinate: String): Int =
+        this.filter { it.first == coordinate }.sumOf { it.second }
+
+    fun part1(): Int {
+
+        val depth =
+            submarine.sumOfCoordinate("down")
+                .minus(submarine.sumOfCoordinate("up"))
+
+        val horizontal = submarine.sumOfCoordinate("forward")
+
+        return depth * horizontal
+    }
+
+    fun part2(): Int {
+
+        var depth = 0
+        var horizontal = 0
+        var aim = 0
+
+        submarine
+            .forEach { x ->
+                when (x.first) {
+                    "down" -> aim += x.second
+                    "up" -> aim -= x.second
+                    "forward" -> {
+                        depth += (aim * x.second)
+                        horizontal += x.second
+                    }
+                }
+            }
+
+        return horizontal * depth
+    }
+}
---
+++ 2021/kotlin/src/test/kotlin/Day2Test.kt
@@ -0,0 +1,31 @@
+package com.codelicia.advent2021
+
+import Util.readInputAsList
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Day2Test {
+
+    @Test
+    fun part1() {
+        assertEquals(150, Day2(SAMPLE_INPUT).part1())
+        assertEquals(1989014, Day2(readInputAsList(2)).part1())
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(900, Day2(SAMPLE_INPUT).part2())
+        assertEquals(2006917119, Day2(readInputAsList(2)).part2())
+    }
+
+    companion object {
+        private val SAMPLE_INPUT = listOf(
+            "forward 5",
+            "down 5",
+            "forward 8",
+            "up 3",
+            "down 8",
+            "forward 2"
+        )
+    }
+}
