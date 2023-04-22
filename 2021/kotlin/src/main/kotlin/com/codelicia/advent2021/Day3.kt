00534f828ad318ec1e9c5dddfbf35244846c4538
Jefersson Nathan <malukenho.dev@gmail.com>
2023-04-22 20:33:36 +0200

    feat(day3): optimize set reduction in part2()
    Refactor the `reduce()` function to be more efficient when reducing
    the set of input strings in `part2()`. Use `rank()` to group and count
    bits for each position in the strings, and then iterate over the set
    of strings to remove those that do not have the expected bit at each
    position. Recursively call `reduce()` until only one string is left
    in the set. No functionality changes.

--- 2021/kotlin/src/main/kotlin/com/codelicia/advent2021/Day3.kt
+++ 2021/kotlin/src/main/kotlin/com/codelicia/advent2021/Day3.kt
@@ -1,7 +1,6 @@
 package com.codelicia.advent2021
 
 import java.util.SortedMap
-import kotlin.math.min
 
 class Day3(private val input: List<String>) {
 
@@ -14,8 +13,19 @@ class Day3(private val input: List<String>) {
 
     private fun List<Int>.epsilon() = this.map(::invert).toInt()
 
-    private fun SortedMap<Int, Int>.bit(): Int = if (this.getValue(0) > this.getValue(1)) 0 else 1
-    private fun SortedMap<Int, Int>.invBit(): Int = if (this.getValue(0) > this.getValue(1)) 1 else 0
+    private fun SortedMap<Int, Int>.bit(): Int =
+        if (this.getValue(0) > this.getValue(1)) 0 else 1
+
+    private fun SortedMap<Int, Int>.invBit(): Int =
+        if (this.getValue(0) > this.getValue(1)) 1 else 0
+
+    private fun MutableSet<String>.rank(n: Int): List<SortedMap<Int, Int>> =
+        listOf(
+            this.map { v -> v[n].digitToInt() }
+                .groupingBy { it }
+                .eachCount()
+                .toSortedMap()
+        )
 
     private val diagnosticReport = buildList {
         repeat(forEachDigit) { i ->
@@ -29,71 +39,40 @@ class Day3(private val input: List<String>) {
         }
     }
 
-    private val grouped = buildList {
-        repeat(forEachDigit) { i ->
-            val ofBit = input
-                .groupingBy { it[i].digitToInt() }
-                .eachCount()
-                .toSortedMap()
-
-            add(ofBit)
-        }
-    }
-
     private fun invert(n: Int) = if (n == 1) 0 else 1
+
+    private fun reduce(
+        s: MutableSet<String>,
+        predicate: (String, Int, List<SortedMap<Int, Int>>) -> Boolean,
+        n: Int = 0,
+    ): Int {
+        if (s.count() == 1) return s.first().toInt(radix = 2)
+
+        val sl = s.rank(n)
+
+        s.toList().forEach { v -> if (predicate(v, n, sl)) s.remove(v) }
+
+        return reduce(s, predicate, n + 1)
+    }
 
     fun part1(): Int = diagnosticReport.gamma() * diagnosticReport.epsilon()
 
     fun part2(): Int {
 
-        fun reduce2(
-            s: MutableSet<String>,
-            n: Int,
-            sl: List<SortedMap<Int, Int>>,
-            predicate: (String, Int, List<SortedMap<Int, Int>>) -> Boolean
-        ): Int {
-            if (s.count() == 1) return s.first().toInt(radix = 2)
+        val l = buildSet { input.forEach(::add) }
 
-            s.toList().forEach { v ->
-                if (predicate(v, n, sl)) s.remove(v)
-            }
-
-            return reduce2(s, n + 1, s.rank(min(s.first().length-1, n + 1)), predicate)
-        }
-
-        println("TOOOOO ${grouped}")
-        val co3 = reduce2(
-            buildSet(input.size) { input.forEach(::add) }.toMutableSet(),
-            0,
-            grouped,
-            fun (v: String, n: Int, sl: List<SortedMap<Int, Int>>): Boolean {
-                return v.getOrElse(n) { '0' }.digitToInt() == sl.get(0).invBit()
-            }
+        val co3 = reduce(
+            l.toMutableSet(),
+            fun(v: String, n: Int, sl: List<SortedMap<Int, Int>>): Boolean =
+                v.getOrElse(n) { '0' }.digitToInt() == sl.get(0).invBit()
         )
-        val ox = reduce2(
-            buildSet(input.size) { input.forEach(::add) }.toMutableSet(),
-            0,
-            grouped,
-            fun (v: String, n: Int, sl: List<SortedMap<Int, Int>>): Boolean {
-                return v.getOrElse(n) { '0' }.digitToInt() == sl.get(0).bit()
-            }
+
+        val ox = reduce(
+            l.toMutableSet(),
+            fun(v: String, n: Int, sl: List<SortedMap<Int, Int>>): Boolean =
+                v.getOrElse(n) { '0' }.digitToInt() == sl.get(0).bit()
         )
-        println(ox)
-        println(co3)
 
         return ox * co3
     }
-}
-
-private fun MutableSet<String>.rank(n: Int): List<SortedMap<Int, Int>> {
-    println("initial list: ${this} on position ${n}")
-    val x = listOf(
-        this.map { v -> v[n].digitToInt() }
-            .groupingBy { it }
-            .eachCount()
-            .toSortedMap()
-    )
-
-    println("shorted list: ${x}")
-    return x
 }
