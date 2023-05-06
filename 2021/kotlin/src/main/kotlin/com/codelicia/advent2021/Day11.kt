445cb27d2dcc82540988e59c4b4fc2a8baebf065
Jefersson Nathan <malukenho.dev@gmail.com>
2023-05-06 17:56:16 +0200

    refactor(day11): remove duplicated logic

--- 2021/kotlin/src/main/kotlin/com/codelicia/advent2021/Day11.kt
+++ 2021/kotlin/src/main/kotlin/com/codelicia/advent2021/Day11.kt
@@ -2,69 +2,20 @@ package com.codelicia.advent2021
 
 class Day11(private val input: List<String>) {
 
-    fun part1(steps: Int): Int {
+    fun part1(steps: Int): Int = solve(steps).first
+
+    fun part2(): Int = solve(100).second
+
+    private fun solve(steps: Int = 10): Pair<Int, Int> {
         val grid = input.map { line ->
-            line.split("").filterNot(String::isBlank).map(String::toInt).toMutableList()
-        }.toMutableList()
+                line.split("").filterNot(String::isBlank).map(String::toInt).toMutableList()
+            }.toMutableList()
 
         var totalFlashes = 0
-
-        repeat(steps) {
-            val notifyNeighbors = ArrayDeque<Pair<Int, Int>>()
-            val blocked = mutableSetOf<Pair<Int, Int>>()
-
-            grid.forEachIndexed { y, line ->
-                line.forEachIndexed { x, value ->
-                    val next = (value + 1) % 10
-                    grid[y][x] = next
-                    if (next == 0) {
-                        notifyNeighbors.add(y to x)
-                        blocked.add(y to x)
-                        totalFlashes++
-                    }
-                }
-            }
-
-            while (notifyNeighbors.isNotEmpty()) {
-                val (y, x) = notifyNeighbors.removeFirst()
-
-                for (neighbor in listOf(
-                    y to x + 1,
-                    y to x - 1,
-                    y - 1 to x,
-                    y + 1 to x,
-                    y + 1 to x + 1,
-                    y - 1 to x - 1,
-                    y + 1 to x - 1,
-                    y - 1 to x + 1,
-                )) {
-                    if (true == blocked.contains(neighbor)) continue
-                    if (grid.getOrNull(neighbor.first)?.getOrNull(neighbor.second) == null) continue
-
-                    val next = (grid[neighbor.first][neighbor.second] + 1) % 10
-                    grid[neighbor.first][neighbor.second] = next
-
-                    if (next == 0) {
-                        notifyNeighbors.add(neighbor)
-                        blocked.add(neighbor)
-                        totalFlashes++
-                    }
-                }
-            }
-        }
-
-        return totalFlashes
-    }
-
-    fun part2(): Int {
-        val grid = input.map { line ->
-            line.split("").filterNot(String::isBlank).map(String::toInt).toMutableList()
-        }.toMutableList()
-
         var loops = 0
-
-        while (grid.flatten().filterNot { it == 0 }.isNotEmpty()) {
+        while (grid.any { it.any { value -> value != 0 } }) {
             loops++
+
             val notifyNeighbors = ArrayDeque<Pair<Int, Int>>()
             val blocked = mutableSetOf<Pair<Int, Int>>()
 
@@ -75,6 +26,7 @@ class Day11(private val input: List<String>) {
                     if (next == 0) {
                         notifyNeighbors.add(y to x)
                         blocked.add(y to x)
+                        if (loops <= steps) totalFlashes++
                     }
                 }
             }
@@ -101,11 +53,12 @@ class Day11(private val input: List<String>) {
                     if (next == 0) {
                         notifyNeighbors.add(neighbor)
                         blocked.add(neighbor)
+                        if (loops <= steps) totalFlashes++
                     }
                 }
             }
         }
 
-        return loops
+        return totalFlashes to loops
     }
 }
