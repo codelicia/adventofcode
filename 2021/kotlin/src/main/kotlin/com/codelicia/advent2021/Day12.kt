bd9278ee8d3db636b7f04c70fc004b78038ee7d5
Jefersson Nathan <malukenho.dev@gmail.com>
2023-05-10 09:19:53 +0200

    refactor(day12): remove duplicated bfs logic

--- 2021/kotlin/src/main/kotlin/com/codelicia/advent2021/Day12.kt
+++ 2021/kotlin/src/main/kotlin/com/codelicia/advent2021/Day12.kt
@@ -6,65 +6,55 @@ class Day12(mapOfTheRemainingCaves: List<String>) {
 
     data class Vertex(val label: String)
 
-    private val graph: MutableMap<Vertex, MutableList<Vertex>> = mutableMapOf()
-
     // @TODO: Maybe build the tree here already?
     private val map = mapOfTheRemainingCaves
         .map { it.split("-") }
         .map { it[0] to it[1] }
 
-    fun part1(): Int {
-        // Build cave connections
+    private val graph = buildMap<Vertex, MutableList<Vertex>> {
         for (i in map) {
-            if (graph[Vertex(i.first)] == null)
-                graph.putIfAbsent(Vertex(i.first), mutableListOf(Vertex(i.second)))
+            if (this[Vertex(i.first)] == null)
+                this.putIfAbsent(Vertex(i.first), mutableListOf(Vertex(i.second)))
             else
-                graph[Vertex(i.first)]?.add(Vertex(i.second))
+                this[Vertex(i.first)]?.add(Vertex(i.second))
 
-            if (graph[Vertex(i.second)] == null)
-                graph.putIfAbsent(Vertex(i.second), mutableListOf(Vertex(i.first)))
+            if (this[Vertex(i.second)] == null)
+                this.putIfAbsent(Vertex(i.second), mutableListOf(Vertex(i.first)))
             else
-                graph[Vertex(i.second)]?.add(Vertex(i.first))
+                this[Vertex(i.second)]?.add(Vertex(i.first))
         }
-
-        val queue = Stack<Vertex>()
-        queue.add(Vertex("start"))
-        val visited = mutableSetOf<String>()
-
-        val results = mutableListOf<String>()
-        while (queue.isNotEmpty()) {
-            go(graph, queue, results, "start", visited)
-        }
-        return results.size
     }
 
-    fun part2(): Int {
-        // Build cave connections
-        for (i in map) {
-            if (graph[Vertex(i.first)] == null)
-                graph.putIfAbsent(Vertex(i.first), mutableListOf(Vertex(i.second)))
-            else
-                graph[Vertex(i.first)]?.add(Vertex(i.second))
-
-            if (graph[Vertex(i.second)] == null)
-                graph.putIfAbsent(Vertex(i.second), mutableListOf(Vertex(i.first)))
-            else
-                graph[Vertex(i.second)]?.add(Vertex(i.first))
-        }
-
+    fun part1(): Int {
         val queue = Stack<Vertex>()
         queue.add(Vertex("start"))
         val visited = hashMapOf<String, Int>()
 
         val results = mutableListOf<String>()
         while (queue.isNotEmpty()) {
-            go2(graph, queue, results, "start", visited)
+            bfs(graph, fun(_: HashMap<String, Int>): Boolean = true, queue, results, "start", visited)
         }
         return results.size
     }
 
-    private fun go2(
-        graph: MutableMap<Vertex, MutableList<Vertex>>,
+    fun part2(): Int {
+        val queue = Stack<Vertex>()
+        queue.add(Vertex("start"))
+        val visited = hashMapOf<String, Int>()
+
+        val results = mutableListOf<String>()
+        val predicate = fun(visited: HashMap<String, Int>): Boolean {
+            return visited.toMap().filter { it.value > 1 }.filter { it.key.lowercase() == it.key }.values.isNotEmpty()
+        }
+        while (queue.isNotEmpty()) {
+            bfs(graph, predicate, queue, results, "start", visited)
+        }
+        return results.size
+    }
+
+    private fun bfs(
+        graph: Map<Vertex, MutableList<Vertex>>,
+        predicate: (HashMap<String, Int>) -> Boolean,
         queue: Stack<Vertex>,
         results: MutableList<String>,
         result: String,
@@ -82,40 +72,12 @@ class Day12(mapOfTheRemainingCaves: List<String>) {
         val vertices = graph[v]!!
         for (i in vertices) {
             if (i.label != "start") {
-                val twice = visited.toMap().filter { it.value > 1 }.filter { it.key.lowercase() == it.key }.values
+                val satisfiesPredicate = predicate(visited)
 
-                val hasVisitedSmallCaveTwice = twice.isNotEmpty()
-
-                if (i.label.lowercase() == i.label && visited.contains(i.label) && hasVisitedSmallCaveTwice == true) continue
+                if (i.label.lowercase() == i.label && visited.contains(i.label) && satisfiesPredicate == true) continue
 
                 queue.add(i)
-                go2(graph, queue, results, "$result,${i.label}", HashMap(visited))
-            }
-        }
-    }
-
-    private fun go(
-        graph: MutableMap<Vertex, MutableList<Vertex>>,
-        queue: Stack<Vertex>,
-        results: MutableList<String>,
-        result: String,
-        visited: MutableSet<String>
-    ) {
-        val v = queue.pop()
-        if (v.label == "end") {
-            results.add(result)
-            return
-        }
-
-        visited.add(v.label)
-
-        val vertices = graph[v]!!
-        for (i in vertices) {
-            if (i.label != "start") {
-                if (i.label.lowercase() == i.label && visited.contains(i.label)) continue
-
-                queue.add(i)
-                go(graph, queue, results, "$result,${i.label}", visited.toMutableSet())
+                bfs(graph, predicate, queue, results, "$result,${i.label}", HashMap(visited))
             }
         }
     }
