7d6968ea40c4f8768939ff96228951d0df64fc57
Jefersson Nathan <malukenho.dev@gmail.com>
2023-05-14 17:01:34 +0200

    feat(day04): use the same logic for part 1 and part 2
    — I've also renamed variables/classes/methods to better reflect the domain

--- 2021/kotlin/src/main/kotlin/com/codelicia/advent2021/Day04.kt
+++ 2021/kotlin/src/main/kotlin/com/codelicia/advent2021/Day04.kt
@@ -1,25 +1,24 @@
 package com.codelicia.advent2021
 
 class Day04(input: String) {
-    private val ON_COMMA = ","
-    private val ON_NEW_LINE = "\n"
-    private val IN_SECTION = "\n\n"
+    private val newline = "\n"
+    private val section = "\n\n"
 
-    private val sections = input.split(IN_SECTION)
+    private val sections = input.split(section)
 
-    private val draws = sections[0].trim().split(ON_COMMA).map(String::toInt)
+    private val draws = sections[0].trim().split(",").map(String::toInt)
 
-    private var bingoCards : List<BingoCard> = buildList {
+    private val cards : List<BingoBoard> = buildList {
         for (i in 1 until sections.size) {
-            val numbers = sections[i].split(ON_NEW_LINE).map { it.split(" ").filter(String::isNotBlank).map(String::toInt) }
+            val numbers = sections[i].split(newline).map { it.split(" ").filter(String::isNotBlank).map(String::toInt) }
 
-            add(BingoCard(numbers.map { row -> row.map { it to false } }))
+            add(BingoBoard(numbers.map { row -> row.map { it to false } }))
         }
     }
 
-    class BingoCard(private var numbers: List<List<Pair<Int, Boolean>>>) {
+    class BingoBoard(private var numbers: List<List<Pair<Int, Boolean>>>) {
 
-        fun markNumber(number: Int) {
+        fun mark(number: Int) {
             numbers = numbers.map {
                 it.map { pair ->
                     if (pair.first == number) pair.first to true else pair
@@ -27,9 +26,9 @@ class Day04(input: String) {
             }
         }
 
-        fun score(): Int = numbers.flatten().filter { !it.second }.sumOf { it.first }
+        fun unmarkedSum(): Int = numbers.flatten().filter { !it.second }.sumOf { it.first }
 
-        fun isBingo(): Boolean {
+        fun hasBingo(): Boolean {
             val diagonal = List(numbers[0].size) { column -> List(numbers[0].size) { numbers[it][column] } }
             val merge = numbers + diagonal
 
@@ -43,35 +42,24 @@ class Day04(input: String) {
         }
     }
 
-    fun part1(): Int {
+    private fun cardsScore(): MutableSet<Pair<Int, Int>> {
+        val winningCards = mutableSetOf<Pair<Int, Int>>()
 
         draws.forEach { numberCalled ->
-            bingoCards.forEach { it.markNumber(numberCalled) }
+            cards.forEach { it.mark(numberCalled) }
 
-            val isBingo = bingoCards.filter { it.isBingo() }
-
-            if (isBingo.isNotEmpty()) return isBingo[0].score() * numberCalled
-        }
-
-        error("No bingo found")
-    }
-
-    fun part2(): Int {
-        val bingoHistoric = mutableSetOf<Pair<Int, Int>>()
-
-        draws.forEach { numberCalled ->
-            bingoCards.forEach { it.markNumber(numberCalled) }
-
-            val isBingo = bingoCards.filter { it.isBingo() }
+            val isBingo = cards.filter { it.hasBingo() }
 
             isBingo.forEach { card ->
-                if (false == bingoHistoric.map { it.first }.contains(card.hashCode())) {
-                    bingoHistoric.add(card.hashCode() to numberCalled * card.score())
+                if (false == winningCards.map { it.first }.contains(card.hashCode())) {
+                    winningCards.add(card.hashCode() to numberCalled * card.unmarkedSum())
                 }
             }
         }
-
-        // TODO: If we change `last()` to `first()` we can solve part 1 as well with the same logic
-        return bingoHistoric.last().second
+        return winningCards
     }
+
+    fun part1(): Int = cardsScore().first().second
+
+    fun part2(): Int = cardsScore().last().second
 }
