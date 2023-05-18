28dd141c7d18be78c775f5df6087a05cb9171f3a
Jefersson Nathan <malukenho.dev@gmail.com>
2023-05-18 18:04:19 +0200

    feat(day14): solve part 2

--- 2021/kotlin/src/main/kotlin/com/codelicia/advent2021/Day14.kt
+++ 2021/kotlin/src/main/kotlin/com/codelicia/advent2021/Day14.kt
@@ -1,36 +1,50 @@
 package com.codelicia.advent2021
 
 class Day14(val input: String) {
-    fun part1(): Int {
-        var polymerTemplate = input.split("\n\n").take(1).first().trim().split("").filterNot { it.isBlank() }
+    fun solve(times: Int): Long {
+        val polymerTemplate = input.split("\n\n").take(1).first().trim().split("").filterNot { it.isBlank() }
 
-        // Mount hashMap
+        // Mount hashMap with values
         val hashMap = HashMap<String, String>()
-        input.split("\n\n").drop(1).first().split("\n")
-            .map { it.split(" -> ") }
-            .map { hashMap[it.first()] = it.last() }
+        input.split("\n\n").drop(1).first().split("\n").map { it.split(" -> ") }.map { hashMap[it.first()] = it.last() }
 
-        val memo = HashMap<String, String>()
+        var count = mutableMapOf<String, Long>()
 
-        var x = ""
-        repeat(40) {
-            println(it)
-            val windowed = polymerTemplate.windowed(2, 1, true)
-            x = windowed
-                .foldIndexed(" ") { index, acc, s ->
-                    val key = windowed[index].joinToString(separator = "")
-                    if (memo.get(key) == null) {
-                        memo.set(key, windowed[index].first() + (hashMap.get(s.joinToString(separator = "")) ?: ""))
-                    }
-
-                    acc + memo.get(key)
-                }
-            polymerTemplate = x.split("").filterNot { it.isBlank() }
+        for (index in polymerTemplate.indices) {
+            // @todo fix issue with the last digit since `*.indices` -1 didn't work
+            if (0 == polymerTemplate.getOrElse(index + 1) { 0 }) {
+                continue
+            }
+            val x = polymerTemplate[index] + polymerTemplate[index + 1]
+            count.putIfAbsent(x, 0)
+            count[x] = count[x]!! + 1;
         }
-        println(x)
 
-        val groups = x.split("").filterNot { it.isBlank() }.groupingBy { it }.eachCount()
+        repeat(times) {
+            val c2 = HashMap<String, Long>()
+            count.forEach { index ->
+                val v = hashMap[index.key]!!
+                val x = index.key[0] + v
+                val y = v + index.key[1]
+                c2.putIfAbsent(x, 0L)
+                c2.putIfAbsent(y, 0L)
+                c2[x] = c2[x]!! + index.value
+                c2[y] = c2[y]!! + index.value
+            }
+            count = c2
+        }
 
-        return groups.maxOf { it.value } - groups.minOf { it.value }
+        val c2 = HashMap<String, Long>()
+        count.forEach { index ->
+            val x = index.key[0].toString()
+            c2.putIfAbsent(x, 0L)
+            c2[x] = c2[x]!! + index.value
+        }
+
+        // Add the last char otherwise we will be off by one
+        // Bene nota: the last char never changes
+        c2[polymerTemplate.last()] = c2[polymerTemplate.last()]!! + 1
+
+        return c2.maxOf { it.value } - c2.minOf { it.value }
     }
 }
--- 2021/kotlin/src/test/kotlin/Day14Test.kt
+++ 2021/kotlin/src/test/kotlin/Day14Test.kt
@@ -8,8 +8,14 @@ class Day14Test {
 
     @Theory
     fun part1() {
-        assertEquals(1588, Day14(SAMPLE_INPUT).part1())
-        assertEquals(4244, Day14(readInput(14)).part1())
+        assertEquals(1588, Day14(SAMPLE_INPUT).solve(10))
+        assertEquals(4244, Day14(readInput(14)).solve(10))
+    }
+
+    @Theory
+    fun part2() {
+        assertEquals(2188189693529, Day14(SAMPLE_INPUT).solve(40))
+        assertEquals(4807056953866, Day14(readInput(14)).solve(40))
     }
 
     companion object {
