c7fa83c93da4718cb5e68214e812ddbb10b1a248
Jefersson Nathan <malukenho.dev@gmail.com>
2023-05-24 20:56:48 +0200

    feat(day15): rename variables to make code clearer

--- 2021/kotlin/src/main/kotlin/com/codelicia/advent2021/Day15.kt
+++ 2021/kotlin/src/main/kotlin/com/codelicia/advent2021/Day15.kt
@@ -3,14 +3,14 @@ package com.codelicia.advent2021
 import kotlin.math.min
 
 class Day15(val input: String) {
-    private val G = input.split("\n").map { it ->
+    private val grid = input.split("\n").map { it ->
         it.toCharArray().map { it.digitToInt() }
     }
 
-    private val R = G.lastIndex
-    private val C = G.last().lastIndex
+    private val maxRow = grid.lastIndex
+    private val maxColumn = grid.last().lastIndex
 
-    fun solve(): Int = minimumPath(0 to 0, mutableMapOf()) - G[0][0]
+    fun solve(): Int = minimumPath(0 to 0, mutableMapOf()) - grid[0][0]
 
     private fun minimumPath(
         cur: Pair<Int, Int>,
@@ -20,10 +20,10 @@ class Day15(val input: String) {
 
         if (cur in dp) return dp[cur]!!
 
-        if (r<0 || r>R || c<0 || c>C) return 1e9.toInt()
-        if (r==R && c==C) return G[r][c]
+        if (r<0 || r>maxRow || c<0 || c>maxColumn) return 1e9.toInt()
+        if (r==maxRow && c==maxColumn) return grid[r][c]
 
-        val ans = G[r][c] + min(minimumPath(r+1 to c, dp), minimumPath(r to c+1, dp))
+        val ans = grid[r][c] + min(minimumPath(r+1 to c, dp), minimumPath(r to c+1, dp))
 
         dp[r to c] = ans
 
