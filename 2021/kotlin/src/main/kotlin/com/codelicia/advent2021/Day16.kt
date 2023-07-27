0af38ecc1fb16fd67bb9782499464270827b6c74
Jefersson Nathan <malukenho.dev@gmail.com>
2023-07-27 16:35:12 +0200

    feat(day16): bytes parsing

---
+++ 2021/kotlin/src/main/kotlin/com/codelicia/advent2021/Day16.kt
@@ -0,0 +1,49 @@
+package com.codelicia.advent2021
+
+import java.util.*
+import kotlin.math.min
+
+class Day16(val input: String) {
+    // @TODO("Can I get rid of this table")
+    private val lookUp: Map<Char, String> = mutableMapOf(
+       '0' to "0000",
+       '1' to "0001",
+       '2' to "0010",
+       '3' to "0011",
+       '4' to "0100",
+       '5' to "0101",
+       '6' to "0110",
+       '7' to "0111",
+       '8' to "1000",
+       '9' to "1001",
+       'A' to "1010",
+       'B' to "1011",
+       'C' to "1100",
+       'D' to "1101",
+       'E' to "1110",
+       'F' to "1111",
+    )
+
+    data class Packet(
+        val version: String,
+        val typeID: Int,
+        val groups: String,
+        val lastBits: String
+    )
+
+    fun part1(): Int {
+
+        val bitsStreaming = input.map { c -> lookUp[c] }.joinToString("")
+
+        // @todo probably use slide window here
+        val message = bitsStreaming.filterIndexed { index, c ->
+            if (index < 6 || index > bitsStreaming.length - 4) return@filterIndexed false;
+            return@filterIndexed true
+        }
+
+        println("1110".toByte(2))
+
+
+        return 2022
+    }
+}
