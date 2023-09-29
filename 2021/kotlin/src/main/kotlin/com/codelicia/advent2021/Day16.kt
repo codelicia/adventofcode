6a37403e6d3343593c9df8943485b7c056576549
Jefersson Nathan <malukenho.dev@gmail.com>
2023-09-29 10:16:54 +0200

    feat(day16): part 1

--- 2021/kotlin/src/main/kotlin/com/codelicia/advent2021/Day16.kt
+++ 2021/kotlin/src/main/kotlin/com/codelicia/advent2021/Day16.kt
@@ -2,135 +2,69 @@ package com.codelicia.advent2021
 
 class Day16(val input: String) {
 
-    // @TODO("Can I get rid of this table")
-    private val lookUp: Map<Char, String> = mutableMapOf(
-       '0' to "0000",
-       '1' to "0001",
-       '2' to "0010",
-       '3' to "0011",
-       '4' to "0100",
-       '5' to "0101",
-       '6' to "0110",
-       '7' to "0111",
-       '8' to "1000",
-       '9' to "1001",
-       'A' to "1010",
-       'B' to "1011",
-       'C' to "1100",
-       'D' to "1101",
-       'E' to "1110",
-       'F' to "1111",
-    )
+    private fun hexToBinary(input: String): List<Char> =
+        input.map { it.digitToInt(16).toString(2).padStart(4, '0') }.flatMap { it.toList() }
 
-    data class Packet(
-        val version: Int,
-        val typeID: Int,
-        val operator: Int?,
-        val biteLength: Int,
-        val literal: Int?,
-        val subPackets: List<Packet>?,
-        val groups: String
-    ) {
-        companion object {
-            private fun String.decodeBinary(): Int = this.toByte(2).toInt()
+    private var transmission: Iterator<Char> = hexToBinary(input).iterator()
 
-            fun of(bitsStreaming: String): Packet {
-                // the following 3 bits are the type ID
-                val typeID: Int = bitsStreaming.take(6).takeLast(3).decodeBinary()
+    fun Iterator<Char>.next(size: Int): String =
+        (1..size).map { next() }.joinToString("")
 
-                if (typeID == 4)  {
-                    return Packet(
-                        // first three bits are the packet version
-                        bitsStreaming.take(3).decodeBinary(),
-                        typeID,
-                        null,
-                        5,
-                        ,
-                        listOf(),
-                        bitsStreaming.takeLast(bitsStreaming.length - 6)
-                    )
-                }
-
-                // 15
-                if (bitsStreaming.take(7).takeLast(1).toInt() == 0) {
-                    val bits = bitsStreaming.take(7 + 15).takeLast(15).decodeBinary()
-                    val subPackets = bitsStreaming.takeLast(bitsStreaming.length - (7 + 15)).take(bits)
-
-
-                    var ll = mutableListOf<Packet>()
-                    var binaryString = ""
-                    var lastGroup = false
-                    var skipAll = false
-                    subPackets.forEachIndexed { i, c ->
-                        if (i != 0 && i % 11 == 0) {
-                            ll.add(Packet.of(binaryString))
-                            if (lastGroup) skipAll = true
-                            if (c == '0') lastGroup = true
-                            return@forEachIndexed
-                        }
-
-                        if (skipAll) return@forEachIndexed
-                        binaryString += c
-                    }
-
-                    val p = Packet(
-                        // first three bits are the packet version
-                        bitsStreaming.take(3).decodeBinary(),
-                        // the following 3 bits are the type ID
-                        bitsStreaming.take(6).takeLast(3).decodeBinary(),
-                        bitsStreaming.take(7).takeLast(1).toInt(),
-                        bitsStreaming.take(7 + 15).takeLast(15).decodeBinary(),
-                        null,
-                        ll,
-                        subPackets
-                    )
-
-                    return p
-                }
-
-                // 11 - number of sub-packages
-
-                val bits = bitsStreaming.take(7 + 11).takeLast(11).decodeBinary()
-
-                val p = Packet(
-                    // first three bits are the packet version
-                    bitsStreaming.take(3).decodeBinary(),
-                    // the following 3 bits are the type ID
-                    bitsStreaming.take(6).takeLast(3).decodeBinary(),
-                    bitsStreaming.take(7 + 11).takeLast(11).decodeBinary(),
-                    if (bitsStreaming.take(7).takeLast(1).toInt() == 0) 15 else 11,
-                    null,
-                    listOf(Packet.of(bitsStreaming.takeLast(bitsStreaming.length - bits))),
-                    bitsStreaming.takeLast(bitsStreaming.length - 7)
-                )
-
-                return p
-            }
-        }
+    fun part1(): Pair<Long, Long> {
+        return getPacket(transmission)
     }
 
-    fun part1(): Int {
+    // Version -> Count
+    private fun getPacket(transmission: Iterator<Char>): Pair<Long, Long> {
+        val version = transmission.next(3).toInt(2)
+        val typeId = transmission.next(3).toInt(2)
 
-        val bitsStreaming = input.map { c -> lookUp[c] }.joinToString("")
+        var versionSum = version.toLong()
+        var countSum = 0L
 
-        val p = Packet.of(bitsStreaming)
+        var count = ""
+        when (typeId) {
+            4 -> {
+                var keepCounting = true;
+                while (keepCounting) {
+                    val lastBit = transmission.next(1)
+                    if (lastBit == "0") keepCounting = false;
 
-        println(p)
-
-        var binaryString = ""
-        var lastGroup = false
-        var skipAll = false
-        p.groups.forEachIndexed { i, c ->
-            if (i == 0 || i % p.biteLength == 0) {
-                if (lastGroup) skipAll = true
-                if (c == '0') lastGroup = true
-                return@forEachIndexed
+                    val rest = transmission.next(4)
+                    count += rest
+                }
             }
 
-            if (skipAll) return@forEachIndexed
-            binaryString += c
+            else -> {
+                val type = transmission.next(1).toInt(2)
+                when (type) {
+                    0 -> { // 15
+                        val subPacketsLength = transmission.next(15).toInt(2)
+
+                        val subPacketBits = transmission.next(subPacketsLength).toCharArray()
+                        val iterator = subPacketBits.iterator()
+
+                        while (iterator.hasNext()) {
+                            val p = getPacket(iterator)
+                            countSum += p.second
+                            versionSum += p.first
+                        }
+                        return versionSum to countSum
+                    }
+                    1 -> { // 11
+                        val subPacketsLength = transmission.next(11).toInt(2)
+
+                        repeat (subPacketsLength) {
+                            val p = getPacket(transmission)
+                            versionSum += p.first
+                            countSum += p.second
+                        }
+                        return versionSum to countSum
+                    }
+                }
+            }
         }
 
-        return binaryString.toInt(2)
+        return versionSum to countSum + count.toLong(2)
     }
 }
--- 2021/kotlin/src/test/kotlin/Day16Test.kt
+++ 2021/kotlin/src/test/kotlin/Day16Test.kt
@@ -8,10 +8,16 @@ class Day16Test {
 
     @Theory
     fun part1() {
-        assertEquals(2021, Day16("D2FE28").part1())
-        assertEquals(16, Day16("38006F45291200").part1())
-//        assertEquals(16, Day16("8A004A801A8002F478").part1())
-//        assertEquals(16, Day16("8A004A801A8002F478").part1())
+        assertEquals(2021, Day16("D2FE28").part1().second)
+        assertEquals(30, Day16("38006F45291200").part1().second)
+        assertEquals(6, Day16("EE00D40C823060").part1().second)
+
+        assertEquals(16, Day16("8A004A801A8002F478").part1().first)
+        assertEquals(12, Day16("620080001611562C8802118E34").part1().first)
+        assertEquals(23, Day16("C0015000016115A2E0802F182340").part1().first)
+        assertEquals(31, Day16("A0016C880162017C3686B18A3D4780").part1().first)
+
+        assertEquals(953, Day16(readInput(16)).part1().first)
     }
 
     companion object {
