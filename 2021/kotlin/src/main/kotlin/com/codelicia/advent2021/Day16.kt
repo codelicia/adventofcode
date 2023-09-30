2337f951d507eb82f021a1df772ed48a14fc2abb
Jefersson Nathan <malukenho.dev@gmail.com>
2023-09-30 10:38:28 +0200

    feature : day 16 part 2

--- 2021/kotlin/src/main/kotlin/com/codelicia/advent2021/Day16.kt
+++ 2021/kotlin/src/main/kotlin/com/codelicia/advent2021/Day16.kt
@@ -10,20 +10,23 @@ class Day16(val input: String) {
     fun Iterator<Char>.next(size: Int): String =
         (1..size).map { next() }.joinToString("")
 
-    fun part1(): Pair<Long, Long> = getPacket(transmission)
+    fun part1(): Long = getPacket(transmission).first
+
+    fun part2(): Long = getPacket(transmission).second
 
     // Version -> Count
     private fun getPacket(transmission: Iterator<Char>): Pair<Long, Long> {
+        val subPackets = mutableListOf<Long>()
+
         val version = transmission.next(3).toInt(2)
         val typeId = transmission.next(3).toInt(2)
 
         var versionSum = version.toLong()
-        var countSum = 0L
 
-        var count = ""
         when (typeId) {
             4 -> {
                 var keepCounting = true;
+                var count = ""
                 while (keepCounting) {
                     val lastBit = transmission.next(1)
                     if (lastBit == "0") keepCounting = false;
@@ -31,6 +34,7 @@ class Day16(val input: String) {
                     val rest = transmission.next(4)
                     count += rest
                 }
+                subPackets.add(count.toLong(2))
             }
 
             else -> {
@@ -44,10 +48,10 @@ class Day16(val input: String) {
 
                         while (iterator.hasNext()) {
                             val p = getPacket(iterator)
-                            countSum += p.second
                             versionSum += p.first
+                            subPackets.add(p.second)
                         }
-                        return versionSum to countSum
+                        return versionSum to subPackets.operateBy(typeId)
                     }
                     1 -> { // 11
                         val subPacketsLength = transmission.next(11).toInt(2)
@@ -55,14 +59,26 @@ class Day16(val input: String) {
                         repeat (subPacketsLength) {
                             val p = getPacket(transmission)
                             versionSum += p.first
-                            countSum += p.second
+                            subPackets.add(p.second)
                         }
-                        return versionSum to countSum
+                        return versionSum to subPackets.operateBy(typeId)
                     }
                 }
             }
         }
 
-        return versionSum to countSum + count.toLong(2)
+        return versionSum to subPackets.operateBy(typeId)
     }
+
+    fun MutableList<Long>.operateBy(id: Int) =
+        when (id) {
+            0 -> sumOf { it }
+            1 -> reduce { acc, cur -> acc * cur }
+            2 -> minOf { it }
+            3 -> maxOf { it }
+            5 -> if (this[0] > this[1]) 1 else 0
+            6 -> if (this[0] < this[1]) 1 else 0
+            7 -> if (this[0] == this[1]) 1 else 0
+            else -> first()
+        }
 }
